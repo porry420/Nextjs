3d8d445ba58adab1d2ec59d05697da446c1009cc
porry420 <porry1657@gmail.com>
2024-06-01 14:56:01 -0700

    fix: build error related to escape character

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -20,14 +20,14 @@ const page = (props: Props) => {
           <h1 className="text-4xl text-center">Lorem Ipsum</h1>
           <p className="text-lg leading-relaxed">
             Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
+            industry. Lorem Ipsum has been the industry&apos;s standard dummy
+            text ever since the 1500s, when an unknown printer took a galley of
+            type and scrambled it to make a type specimen book. It has survived
+            not only five centuries, but also the leap into electronic
+            typesetting, remaining essentially unchanged. It was popularised in
+            the 1960s with the release of Letraset sheets containing Lorem Ipsum
+            passages, and more recently with desktop publishing software like
+            Aldus PageMaker including versions of Lorem Ipsum.
           </p>
         </div>
       </div>
