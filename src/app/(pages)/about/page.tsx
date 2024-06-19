f689ac4f3bebae9fe79e91e70d5a1e3efc2741d4
porry420 <porry1657@gmail.com>
2024-06-18 18:23:25 -0700

    fix: more updates to about page text positioning

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -6,9 +6,9 @@ type Props = {};
 
 const page = (props: Props) => {
   return (
-    <div className="flex w-full h-screen  lg:min-h-[calc(100vh-80px)]  lg:mt-0 px-[5%] lg:px-0">
+    <div className="flex w-full h-screen  lg:min-h-[calc(100vh-80px)]  lg:mt-0 px-[2.5%] lg:px-0">
       <div className="m-auto flex h-full justify-center mx-auto w-full py-12 max-w-7xl lg:flex-row flex-col gap-4 lg:gap-0">
-        <div className="relative z-0 mt-6 lg:mt-auto  justify-end my-auto order-2 w-[80%] lg:w-1/2  m-auto lg:mx-0 h-full md:h-[80%] xl:h-[90%] ">
+        <div className="relative z-0 mt-6 lg:mt-auto  justify-end my-auto order-2 w-[90%] lg:w-1/2  m-auto lg:mx-0 h-full md:h-[80%] xl:h-[90%] ">
           <ImgWithSkeleton
             src="/about-image.jpg"
             alt="About us"
@@ -30,7 +30,7 @@ const page = (props: Props) => {
               backgroundPosition: "100% 100%",
             }}></div>{" "}
         </div>
-        <div className="lg:w-1/2 lg:h-[80%]  my-auto dark:text-white px-4  flex flex-col lg:order-2">
+        <div className="lg:w-1/2 lg:h-[85%]  my-auto dark:text-white px-1  flex flex-col lg:order-2">
           <div className="mx-auto w-full gap-6 pl-4 flex flex-col">
             <h1 className="text-4xl lg:text-left text-center uppercase">
               Who we are
